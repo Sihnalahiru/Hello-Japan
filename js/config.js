458be3ad807f475512b93e3610a62eabf491a4c3
Sihina Lahiru <149120815+Sihnalahiru@users.noreply.github.com>
2026-09-22 10:36:51 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,5 @@
-window.App = window.App || {};
-
-App.Config = {
-    WORKER_ENDPOINT: "https://hello-japan.madusanka-lahiru.workers.dev/",
+export const Config = {
+    WORKER_ENDPOINT: "[https://hello-japan.madusanka-lahiru.workers.dev/](https://hello-japan.madusanka-lahiru.workers.dev/)",
     DEFAULT_TIMEOUT_MS: 12000,
     OCR_TIMEOUT_MS: 18000,
     MAX_IMAGE_DIMENSION: 1280
