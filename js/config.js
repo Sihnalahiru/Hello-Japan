4580de6e1873ce864d82953d04ca6d03ec12a971
Sihina Lahiru <149120815+Sihnalahiru@users.noreply.github.com>
2026-09-22 11:43:48 +0530

    Fix WORKER_ENDPOINT formatting in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const Config = {
-    WORKER_ENDPOINT: "[https://hello-japan.madusanka-lahiru.workers.dev/](https://hello-japan.madusanka-lahiru.workers.dev/)",
+    WORKER_ENDPOINT: "https://hello-japan.madusanka-lahiru.workers.dev/",
     DEFAULT_TIMEOUT_MS: 12000,
     OCR_TIMEOUT_MS: 18000,
     MAX_IMAGE_DIMENSION: 1280
